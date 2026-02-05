b8054bb299d7597602a659aa1ab2116a1ee509d5
dhanushree <dhanushree08180@gmail.com>
2026-02-05 19:35:39 +0530

    Core updation

--- app.py
+++ app.py
@@ -719,3 +719,4 @@ def form_summary(form_id):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
+# ================= HELPER: send verification email =================
